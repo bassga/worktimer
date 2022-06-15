171a8f67898eb6969375f9835ccbdb7bd3624719
basega <bassmky19940208@gmail.com>
2022-06-15 17:42:05 +0900

    Initial Commit

---
+++ worktimer/worktimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  worktimerApp.swift
+//  worktimer
+//
+//  Created by 奥川幹也 on 2022/06/15.
+//
+
+import SwiftUI
+
+@main
+struct worktimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
